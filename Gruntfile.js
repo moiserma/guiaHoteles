ba50eec7ce321ba51025ed558ac9c0835703d544
Moises Serrano Marin <69829413+moiserma@users.noreply.github.com>
2020-09-01 16:56:43 -0600

    usando grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,12 @@
+const imagemin = require('gulp-imagemin');
+
 module.exports = function (grunt){
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt , {
+        useminPrepare: 'grunt-usemin'
+    });  
+    
+    
     grunt.initConfig({
         sass: {
             dist: {
@@ -36,10 +44,136 @@ module.exports = function (grunt){
             }
         },
 
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    src: 'img/*.{png, gif, jpg, jpeg}',
+                    dest: 'dist/'
+                }]
+            }
+        },
+
+        copy:{
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [
+                    {
+                    //for font-awesome
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/open-iconic/font',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                    }
+                ]
+            },
+        },
+
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+
+        cssmin: {
+            dist: {}
+        },
+
+        uglify: {
+            dist: {}
+        },
+
+        filerev: {
+            options: {
+                encoding: 'utf-8',
+                algoritm: 'md5',
+                length: 20
+            },
+            release:{
+                // filerev: release hashes(md5) all assets (images, js and css)
+                // in dist directory
+                files:[{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css',
+                    ]
+                }]
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {}
+        },
+
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['index.html', 'about.html', 'precios.html', 'contacto.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block){
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0,
+                                    rebase: false
+                                }
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+
+        usemin: {
+            html: ['dist/index.html', 'dist/about.html', 'dist/precios.html', 'dist/contacto.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css', 'dist/js']
+            }
+        }
     });
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-browser-sync');
+
     grunt.registerTask('css', ['sass']);
-    grunt.registerTask('default', ['browserSync', 'watch'])
+    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('img:compress', ['imagemin']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+
+    ])
+    
+    
 };
+
+
+/*     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-browser-sync'); */
