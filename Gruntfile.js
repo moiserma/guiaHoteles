a9452be7288cc44f8afd94ce348c45217e87ab63
Moises Serrano Marin <69829413+moiserma@users.noreply.github.com>
2020-09-01 15:44:32 -0600

    implementacion grunt

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function (grunt){
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: [{
+                    expand:  true,
+                    cwd: 'css',
+                    src: ['*.scss'],
+                    dest: 'css',
+                    ext: '.css',
+                }]
+            }
+        },
+
+        watch: {
+            files: ['css/*.scss'],
+            tasks: ['css'],
+
+        },
+        
+        browserSync:{
+            dev: {
+                bsFiles: { //browser files
+                    src: [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './' //Directorio base de nuestro servidor
+                    }
+                }
+            }
+        },
+
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch'])
+};
---
+++ js/index.js
@@ -0,0 +1,30 @@
+; /* Se recomienda los scrips escritos por nosotros referenciarlos y colocarlos en una carpeta Js */
+$(function(){
+    $("[data-toggle='tooltip']").tooltip();
+    $("[data-toggle='popover']").popover();
+    $('.carousel').carousel({
+        interval: 3000
+    });
+
+    $('#contacto').on('show.bs.modal', function(e){
+        console.log('El modal contacto se esta mostrando');
+
+        $('#contactoBtn').removeClass('btn-outline-success');
+        $('#contactoBtn').addClass('btn-secondary');
+        $('#contactoBtn').prop('disabled',true);
+
+
+    });
+    $('#contacto').on('shown.bs.modal', function(e){
+        console.log('El modal contacto se mostró');
+    });
+
+    $('#contacto').on('hide.bs.modal', function(e){
+        console.log('El modal contacto se oculta');
+    });
+    $('#contacto').on('hidden.bs.modal', function(e){
+        console.log('El modal contacto se ocultó');
+        $('#contactoBtn').prop('disabled',false);
+    });
+
+});
